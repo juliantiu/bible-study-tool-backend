eee53fca1d23ed3e5ef507ad83f2edce16a81c9d
Julian Tiu <jtiu.code@gmail.com>
2025-06-10 11:14:08 -0500

    forgotten stage from previous commit. Should be lumped with previous commit.

--- BibleStudyTool.Infrastructure/DAL/EF/BibleReadingDbContext.cs
+++ BibleStudyTool.Infrastructure/DAL/EF/BibleReadingDbContext.cs
@@ -35,7 +35,7 @@ namespace BibleStudyTool.Infrastructure.DAL.EF
         {
         }
 
-        /*public DbSet<BibleVerse> BibleVerse { get; set; }*/
+        public DbSet<BibleVerse> BibleVerse { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -49,9 +49,9 @@ namespace BibleStudyTool.Infrastructure.DAL.EF
             builder.Entity<IdentityUserToken<string>>(b => b.ToTable("asp_net_user_tokens"));
 
             builder.Entity<BibleReader>(b => b.ToTable("bible_readers"));
-            
-            /*builder.Entity<BibleVerse>(b => b.ToTable("bible_verses"));*/
-            
+
+            builder.Entity<BibleVerse>(b => b.ToTable("bible_verses"));
+
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
